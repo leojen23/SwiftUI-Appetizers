72fe65b5697bdd73fd3a7452e87f152a832e61d8
leojen23 <leojen23@hotmail.com>
2023-10-12 14:01:09 +0200

    feat : create Acccount Settings form to store user preferences

--- Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,38 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationView {
-            Text("Account screen")
-                .navigationTitle("😉 Account")
+            Form {
+                Section(header: Text("Personal Info")) {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("E-mail", text: $email)
+                        .keyboardType(.emailAddress)
+                        .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                        .disableAutocorrection(true)
+                    DatePicker("Birthday", selection: $birthDate, displayedComponents: .date)
+                    Button(action: {
+                        print("Save")
+                    }, label: {
+                        Text("Save Settings")
+                    })
+                }
+                
+                Section(header: Text("Requests")) {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                }.toggleStyle(SwitchToggleStyle(tint: Color.brandPrimary))
+            }
+            .navigationTitle("😉 Account")
         }
     }
 }
